0ab3d3bee16f527412b4ed95e50f6d92b1eac565
hmongdev <steveyangwork@gmail.com>
2024-03-09 16:11:03 -0600

    correct imports for authOptions

--- pages/api/pusher/auth.ts
+++ pages/api/pusher/auth.ts
@@ -1,4 +1,4 @@
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { authOptions } from '@/app/api/auth/authOptions';
 import { pusherServer } from '@/app/lib/pusher';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getServerSession } from 'next-auth';
