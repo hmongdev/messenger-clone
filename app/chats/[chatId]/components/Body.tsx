bebde9787c62bda710ffb4d13cebed2057a50b86
hmongdev <steveyangwork@gmail.com>
2024-03-08 19:48:06 -0600

    successfully added pusher to body; messages are automatically loaded from pusher now!

--- app/chats/[chatId]/components/Body.tsx
+++ app/chats/[chatId]/components/Body.tsx
@@ -4,8 +4,10 @@
 import { useEffect, useRef, useState } from 'react';
 
 import useChat from '@/app/lib/hooks/useChat';
+import { pusherClient } from '@/app/lib/pusher';
 import { FullMessageType } from '@/app/types';
 import axios from 'axios';
+import { find } from 'lodash';
 import MessageBox from './MessageBox';
 
 interface BodyProps {
@@ -22,51 +24,55 @@ export default function Body({ initialMessages = [] }: BodyProps) {
 		axios.post(`/api/chats/${chatId}/seen`);
 	}, [chatId]);
 
-	// useEffect(() => {
-	// 	pusherClient.subscribe(chatId);
-	// 	bottomRef?.current?.scrollIntoView();
+	useEffect(() => {
+		// first user has to subscribe to pusher
+		pusherClient.subscribe(chatId);
+		bottomRef?.current?.scrollIntoView();
 
-	// 	const messageHandler = (message: FullMessageType) => {
-	// 		axios.post(`/api/chats/${chatId}/seen`);
+		// receives new messages from pusher
+		const messageHandler = (message: FullMessageType) => {
+			// alert other users that we have seen the message
+			axios.post(`/api/chats/${chatId}/seen`);
 
-	// 		setMessages((current) => {
-	// 			if (find(current, { id: message.id })) {
-	// 				return current;
-	// 			}
+			setMessages((current) => {
+				// compare messages to make sure we don't duplicate them
+				if (find(current, { id: message.id })) {
+					return current;
+				}
 
-	// 			return [...current, message];
-	// 		});
+				return [...current, message];
+			});
+			// scrolls to the bottom
+			bottomRef?.current?.scrollIntoView();
+		};
 
-	// 		bottomRef?.current?.scrollIntoView();
-	// 	};
+		const updateMessageHandler = (newMessage: FullMessageType) => {
+			setMessages((current) =>
+				current.map((currentMessage) => {
+					if (
+						currentMessage.id ===
+						newMessage.id
+					) {
+						return newMessage;
+					}
 
-	// 	const updateMessageHandler = (newMessage: FullMessageType) => {
-	// 		setMessages((current) =>
-	// 			current.map((currentMessage) => {
-	// 				if (
-	// 					currentMessage.id ===
-	// 					newMessage.id
-	// 				) {
-	// 					return newMessage;
-	// 				}
+					return currentMessage;
+				})
+			);
+		};
 
-	// 				return currentMessage;
-	// 			})
-	// 		);
-	// 	};
+		pusherClient.bind('messages:new', messageHandler);
+		pusherClient.bind('message:update', updateMessageHandler);
 
-	// 	pusherClient.bind('messages:new', messageHandler);
-	// 	pusherClient.bind('message:update', updateMessageHandler);
-
-	// 	return () => {
-	// 		pusherClient.unsubscribe(chatId);
-	// 		pusherClient.unbind('messages:new', messageHandler);
-	// 		pusherClient.unbind(
-	// 			'message:update',
-	// 			updateMessageHandler
-	// 		);
-	// 	};
-	// }, [chatId]);
+		return () => {
+			pusherClient.unsubscribe(chatId);
+			pusherClient.unbind('messages:new', messageHandler);
+			pusherClient.unbind(
+				'message:update',
+				updateMessageHandler
+			);
+		};
+	}, [chatId]);
 
 	return (
 		<div className="flex-1 overflow-y-auto">
